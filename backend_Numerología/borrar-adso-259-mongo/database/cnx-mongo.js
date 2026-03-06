092a393886fe99af81abab73e9baf58d2c643fda
DEV Jefferson <125227918+studentcs502023@users.noreply.github.com>
2026-03-05 19:40:04 -0500

    Delete backend_Numerología/borrar-adso-259-mongo directory

--- backend_Numerología/borrar-adso-259-mongo/database/cnx-mongo.js
+++
@@ -1,9 +0,0 @@
-
-import mongoose from "mongoose";
-
-export const conectarMongo = () => {
-  mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log("BD conectada a MongoDB Atlas"))
-    .catch(err => console.error(err));
-};
